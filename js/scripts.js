08c3d3c143466548abb2e80d8bd8b35dd0afd7eb
Guest User <student@epicodus.com>
2015-06-05 09:25:50 -0700

    add calculateCost method to pizza based on size and topping cost

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,19 @@ Pizza.prototype.addTopping = function(topping) {
   this.toppings.push(topping);
 }
 
+Pizza.prototype.calculateCost = function() {
+  var sizeCosts = {
+    "small": 6,
+    "medium": 10,
+    "large": 14
+  }
+
+  var pizzaCost = sizeCosts[this.size];
+  this.toppings.forEach(function(topping) { pizzaCost += topping.cost; });
+
+  return pizzaCost;
+}
+
 function Topping(type, cost) {
   this.type = type;
   this.cost = cost;
--- spec/specs.js
+++ spec/specs.js
@@ -13,6 +13,22 @@ describe("Pizza", function() {
       expect(pizza.toppings).to.eql([topping]);
     });
   });
+
+  describe("#calculateCost", function() {
+    it("calculates cost based on pizza size", function() {
+      var pizza = new Pizza("medium");
+      expect(pizza.calculateCost()).to.equal(10);
+    });
+
+    it("adds on the cost of each pizza topping", function() {
+      var pizza = new Pizza("small");
+      var topping1 = new Topping("pepperoni", 2);
+      var topping2 = new Topping("mushrooms", 1.5);
+      pizza.addTopping(topping1);
+      pizza.addTopping(topping2);
+      expect(pizza.calculateCost()).to.equal(9.5);
+    });
+  });
 });
 
 describe("Topping", function() {
